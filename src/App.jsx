205aca4b94c8bf436914a2512dde5e6c9fb9bc21
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2023-07-25 12:39:44 +0430

    uodate app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
-import { useState } from 'react'
+import React from 'react'
 import Data from './data'
+import {nanoid} from "nanoid"
 
 
-function App() {
-
+export default function App() {
+  const [dice ,setDice]=React.useState(allNewDice())
+  function allNewDice(){
+    const newDice = []
+    for(let i=0;i<10;i++){
+      newDice.push({value : Math.ceil(Math.random() * 6),
+      isHeld : false,
+      id :nanoid()})
+    }
+    return newDice
+  }
+  function rollDice(){
+    setDice(allNewDice())
+  }
+  const diceElements = dice.map(die => <Data key={die.id} value={die.value}/>)
 
   return (
     <main>
       <div className='container'>
-        <Data value ="1"/>
-        <Data value ="1"/>
-        <Data value ="1"/>
-        <Data value ="1"/>
-        <Data value ="1"/>
-        <Data value ="1"/>
-        <Data value ="1"/>
-        <Data value ="1"/>
-        <Data value ="1"/>
-        <Data value ="1"/>
+        {diceElements}
       </div>
+      <button className='btn' onClick={rollDice}>Roll</button>
     </main>
   )
 }
 
-export default App
+
