ab20d8c073971e284d41c66de73802a5cf05bcd8
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2023-07-26 12:23:24 +0430

    final update

--- src/data.jsx
+++ src/data.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 export default function Data(props){
+    const styles = { backgroundColor : props.isHeld ? "#59E391" : "white" }
     return(
-        <div className="data-face">
+        <div className="data-face" style={styles} onClick={props.holdDice}>
             <h2 className="data-num">{props.value}</h2>
         </div>
     )
